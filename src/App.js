1494261fee880733bd61774908a63bc466a1d941
hasnacreate <pererahasini575@gmail.com>
2024-09-14 17:14:16 +0530

    format

--- src/App.js
+++ src/App.js
@@ -7,29 +7,29 @@ import SignUp from './components/SignUp';
 
 function App() {
   const location = useLocation();
-  const hideNavPages = ['/login', '/signUp', '/todos'];
+  const hideNavPages = ["/login", "/signUp", "/todos"];
   return (
     <div className="App">
-    {/* Conditionally render the nav /*/}
-    {!hideNavPages.includes(location.pathname) && (
-      <nav>
-        <ul>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/signUp">SignUp</Link>
-          </li>
-          <li>
-            <Link to="/todos">Todo List</Link>
-          </li>
-        </ul>
-      </nav>
-    )}
+      {/* Conditionally render the nav /*/}
+      {!hideNavPages.includes(location.pathname) && (
+        <nav>
+          <ul>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signUp">SignUp</Link>
+            </li>
+            <li>
+              <Link to="/todos">Todo List</Link>
+            </li>
+          </ul>
+        </nav>
+      )}
       <Routes>
-      <Route path="/" element={<Navigate to="/signUp" replace />} />
+        <Route path="/" element={<Navigate to="/signUp" replace />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signUp" element={<SignUp />} /> 
+        <Route path="/signUp" element={<SignUp />} />
         <Route path="/todos" element={<TodoList />} />
       </Routes>
     </div>
